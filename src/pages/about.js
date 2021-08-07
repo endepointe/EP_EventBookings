1fadb0d42113751f61fae62cbd99ff399a401579
endepointe <endepointe@gmail.com>
2021-08-07 12:16:39 -0700

    testing

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AboutPage = () => {
+	return (
+		<div>about page</div>
+	);
+}
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import * as React from "react"
 import { useState } from 'react';
+import { Link } from 'gatsby';
 import Layout from "../components/layout";
 import SphereVideo from "../assets/video.mp4";
 import * as styles from "../components/styles/index.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faAngleRight,
-  faBars, faTimes
+  faBars, faLock, faTimes
 } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
@@ -27,13 +28,14 @@ const IndexPage = () => {
           <h2 className={styles.logo}>Event Bookings</h2>   
           <FontAwesomeIcon 
             onClick={openMenu}
-            className={styles.barsIcon}
-            icon={faBars} size="lg" />   
+            className={menu ? `${styles.hide}`: `${styles.barsIcon}` }
+            icon={faBars} />   
         </header>
 
-        {/* <video muted loop autoPlay>
+        <video muted loop autoPlay>
           <source src={SphereVideo} /> 
-        </video> */}
+        </video>
+        {/* Video by Pressmaster from Pexels */}
 
         <div className={styles.overlay}></div>
 
@@ -41,13 +43,14 @@ const IndexPage = () => {
           <div className={styles.text}>
             <h2>Keep pushing</h2> 
             <h3>Foreward</h3>
+            <p>Find your event today.</p>
           </div>
           <div>
-            <p>Find your event today</p>
+            {/* <p className={styles.tester}>Find your event today</p> */}
             <button 
               onClick={test}
               className={styles.arrowBtn}>
-              <div className={styles.arrowText}>Explore</div>
+              <div className={styles.arrowText}>Start Here</div>
               <div className={styles.arrows}>
                 <FontAwesomeIcon 
                   className={styles.arrow}
@@ -65,17 +68,28 @@ const IndexPage = () => {
 
         <ul className={styles.social}>
           <li>
-            <FontAwesomeIcon 
-              className={styles.socialIcon}
-              icon={faTwitter} />
             <a href="twitter.com">
-              twitter</a>
-            </li> 
+              <FontAwesomeIcon 
+                className={styles.socialIcon}
+                icon={faTwitter} />
+              Twitter</a>
+          </li> 
           <li>
-            <FontAwesomeIcon 
-              className={styles.socialIcon}
-              icon={faFacebook} />
-            <a href="facebook.com">facebook</a></li> 
+            <a href="facebook.com">
+              <FontAwesomeIcon 
+                className={styles.socialIcon}
+                icon={faFacebook} />  
+              Facebook
+            </a>
+          </li> 
+          <li>
+            <a href="#">
+              <FontAwesomeIcon 
+                className={styles.socialIcon}
+                icon={faLock} />  
+              Privacy
+            </a>
+          </li> 
         </ul>
       </section>
 
@@ -85,15 +99,27 @@ const IndexPage = () => {
           : `${styles.menu}`}>
         <FontAwesomeIcon 
           className={styles.closeIcon}
-          onClick={openMenu} icon={faTimes} color="black" />
+          onClick={openMenu} icon={faTimes} color="white" />
         <ul className={styles.menuList}>
-          <li>link</li>
-          <li>link</li>
-          <li>link</li>
+          <li>
+            <Link 
+              className={styles.link}
+              to="/account">Account</Link>
+          </li>
+          <li>
+            <Link 
+              className={styles.link}
+              to="/about">About</Link>
+          </li>
+          <li>
+            <Link 
+              className={styles.link}
+              to="/contact">Contact</Link>
+            </li>
         </ul>
       </div>
     </Layout>
   )
 }
 
-export default IndexPage
+export default IndexPage;
