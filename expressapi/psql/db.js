d7d12d56c0f63419c5914e3ed7780a93bf8d15f6
endepointe <endepointe@gmail.com>
2021-08-12 08:35:53 -0700

    get db and table ready for auth testing

--- expressapi/psql/db.js
+++ expressapi/psql/db.js
@@ -1,5 +1,8 @@
 const pgp = require('pg-promise')();
-
+/**
+ * https://www.digitalocean.com/community/tutorials/how-to-create-remove-manage-tables-in-postgresql-on-a-cloud-server
+ * https://www.postgresql.org/docs/12/sql-insert.html
+ */
 const cn = {
 	host: process.env.EB_HOST,
 	port: 5432,
