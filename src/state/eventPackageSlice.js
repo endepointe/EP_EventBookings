937c3798d4fabbb76937d6b1506110edc2c31735
endepointe <endepointe@gmail.com>
2021-09-08 12:55:36 -0700

    tab issue still

---
+++ src/state/eventPackageSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  eventPackages: [],
+  status: 'idle',
+  error: null,
+};
+
+/*
+https://redux.js.org/tutorials/essentials/part-5-async-logic
+*/
+
+const eventPackageSlice = createSlice({
+  name: 'packages',
+  initialState,
+  reducers: {
+    populateEventPackages(state,action) {
+      state.eventPackages.push(action.payload);
+    }
+  }
+});
+
+export const {populateEventPackages} = eventPackageSlice.actions;
+export default eventPackageSlice.reducer;
+export const getEventPackages = state => state.packages.eventPackages;
